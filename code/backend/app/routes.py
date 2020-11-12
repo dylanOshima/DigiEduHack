4f0aa0137459ddc6c81c192a8483584704546256
Apurv Mishra <me@apurvmsihra.xyz>
2020-11-12 13:30:56 +0000

    setup firebase, add routes and changed requirements

--- code/backend/app/routes.py
+++ code/backend/app/routes.py
@@ -1,7 +1,40 @@
 from app import app
+from uuid import uuid4
 from time import time
 
+from flask import request, jsonify
+from firebase_admin import credentials, auth, firestore, initialize_app
+
+
+# Connect to firebase
+cred = credentials.Certificate("fbAdminConfig.json")
+firebase = initialize_app(cred)
+db = firestore.client()
+sessions_ref = db.collection("Sessions")
 
 @app.route('/time')
 def home():
     return {'time': time()}
+
+@app.route('/create-session', methods=['POST'])
+def create_session():
+    post_data = request.get_json()
+    token = str(uuid4())[:8]
+    try:
+        sessions_ref.document(token).set(post_data)
+    except Exception as e:
+            print(e)
+            return jsonify(message="There was an error in creating session"), 400
+    return jsonify(token=token), 200
+
+@app.route('/update-session', methods=['PUT'])
+def create_session():
+    # Expects token to be sent in the json
+    post_data = request.get_json()
+    try:
+        token = post_data.pop("token")
+        sessions_ref.document(token).update(post_data)
+    except Exception as e:
+            print(e)
+            return jsonify(message="There was an error in updating session"), 400
+    return jsonify(token=token), 200
