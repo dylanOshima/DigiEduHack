32ec96fb83e56b09f2ad12cd913d8c3750a7a111
dylanOshima <dylanoshima@gmail.com>
2020-11-12 23:36:53 +0000

    Adds video rtc functionality

---
+++ code/backend-video/server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const https = require('http');
+
+const app = express();
+const PORT = 3000;
+const SignalServer = require('./lib/SignalServer');
+
+app.get('/', (req, res) => {
+  res.sendFile(express.static('../public'));
+});
+// Adding in a express server to HTTPS to allow HTTPS
+const server = https.createServer(
+  // {
+  //   key: fs.readFileSync(path.resolve(__dirname, './ssl_diane/server.key')),
+  //   // // is required to allow localhost to run on HTTPS
+  //   // // otherwise you either get an error or a certified invalid warning
+  //   cert: fs.readFileSync(path.resolve(__dirname, './ssl_diane/server.crt')),
+  // },
+  app,
+);
+
+// WEBSOCKET SECURED CONNECTION //
+// const wss = new WebSocket.Server({ server });
+const signal = new SignalServer({ server });
+signal.connect();
+// const wss = new WebSocket.Server({ port: PORT });
+
+server.listen(PORT, () => {
+  console.log('\nListening on PORT: ', PORT);
+});
