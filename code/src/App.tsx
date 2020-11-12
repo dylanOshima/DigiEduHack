24a614feaf6e0b9228ecde5f18191d1002de48f9
vrambo8 <vittotre@gmail.com>
2020-11-12 13:24:05 +0000

    Started Join Session View

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+import './styles/App.css';
+import {Logo} from "./Logo";
+import {JoinView} from "./JoinView";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>StudySesh</h1>
-      </header>
-      <div className="App-menu">
-        <button onClick={() => console.log("Hehe")}>
-          Start a sesh
-        </button>
-        <button onClick={() => console.log("U have no friends")}>
-          Join a sesh
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <div className="App">
+                        <Logo/>
+                        <div className="App-menu">
+                            <button onClick={() => console.log("Hehe")}>
+                                <Link to="/start">Start a Sesh</Link>
+                            </button>
+                            <button onClick={() => console.log("U have no friends")}>
+                                <Link to="/join">Join a Sesh</Link>
+                            </button>
+                        </div>
+                    </div>
+                </Route>
+                <Route path="/start">
+                    <h1>Start Ma Dick</h1>
+                </Route>
+                <Route path="/join">
+                    <JoinView/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ code/src/JoinView.tsx
@@ -0,0 +1,48 @@
+import React, {FormEvent, useState} from "react";
+import {Logo} from "./Logo";
+import {Button, Grid, TextField} from "@material-ui/core";
+
+import './styles/JoinView.css'
+
+export function JoinView() {
+    const [sessionId, setSessionId]: [string, (arg: string) => void] = useState("");
+    const [username, setUsername]: [string, (arg: string) => void] = useState("");
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(sessionId);
+        console.log(username);
+    }
+    return (
+        <div className="App">
+            <Logo/>
+            <h3>Join a Sesh</h3>
+            <form className="form-group" onSubmit={onSubmit} autoComplete="off">
+                <Grid container spacing={1}>
+                    <Grid container item spacing={1} xs={12} md={12}>
+                        <Grid item md={6} xs={6}>
+                            <h5>Sesh Number</h5>
+                        </Grid>
+                        <Grid item md={6} xs={6} className="vertical-center">
+                            <TextField id="sessionId" variant="outlined" value={sessionId}
+                                       onChange={(e) => setSessionId(e.target.value)}/>
+                        </Grid>
+                    </Grid>
+                    <Grid container item spacing={2} xs={12} md={12}>
+                        <Grid item md={6} xs={6}>
+                            <h5>Your Name</h5>
+                        </Grid>
+                        <Grid item md={6} xs={6} className="vertical-center">
+                            <TextField id="username" variant="outlined" value={username}
+                                       onChange={(e) => setUsername(e.target.value)}/>
+                        </Grid>
+                    </Grid>
+                    <Grid item md={12} xs={12} className="App-menu">
+                        <button className="submit" type="submit"> Join</button>
+                    </Grid>
+                </Grid>
+                <Button/>
+            </form>
+        </div>
+    )
+}
