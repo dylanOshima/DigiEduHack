0d8bf0027827e5e440da55ebdbf67b28667a31f7
Lorenzo <lollobaldo2000@gmail.com>
2020-11-13 03:43:38 +0000

    add answer and vote question

--- code/src/firebase.ts
+++ code/src/firebase.ts
@@ -3,6 +3,7 @@ import firebase from 'firebase';
 import { firebaseConfig } from './fbconfig';
 
 import { GameState } from "./models/GameState";
+import { Answer } from "./models/Question";
 import { mockGameState } from "./models/MockData";
 
 // Initialize Cloud Firestore through Firebase
@@ -31,27 +32,25 @@ export const createSession = () => {
         });
 };
 
-export const getGameData = (id: string) => {return(
+export const getGameData = (id: string) => (
     db.collection("sessions")
         .doc(id)
         .get()
         .then(doc => {
             const data = doc.data();
             return data;
-        }));
-};
+        })
+);
 
-export const joinSession = (id: string, user: string) => {
-    return (
-        getGameData(id)
-            .then((data: any) => {
-                const { users } = data;
-                db.collection("sessions")
-                    .doc(id)
-                    .update({ users: [...users, user]})
-            })
-    );
-};
+export const joinSession = (id: string, user: string) => (
+    getGameData(id)
+        .then((data: any) => {
+            const { users } = data;
+            db.collection("sessions")
+                .doc(id)
+                .update({ users: [...users, user]})
+        })
+);
 
 export const subscribe = (id: string, callback: (arg: any) => void) => (
     db.collection("sessions").doc(id).onSnapshot((doc) => {
@@ -60,3 +59,29 @@ export const subscribe = (id: string, callback: (arg: any) => void) => (
         callback(doc.data());
     })
 );
+
+export const answerQuestion = (id: string, answer: Answer) => (
+    getGameData(id)
+        .then((data: any) => {
+            const { answers } = data;
+            db.collection("sessions")
+                .doc(id)
+                .update({ answers: [...answers, answer]})
+        })
+);
+
+const voteAnswer = (id: string, answer: Answer, change: number) => (
+    getGameData(id)
+        .then((data: any) => {
+            const n = data.answers.map((a: Answer) => {
+                if (a.user === answer.user) { return {...a, votes: a.votes+change}; }
+                return a;
+            });
+            db.collection("sessions")
+                .doc(id)
+                .update({ answers: [...n]})
+        })
+);
+
+export const upvoteAnswer = (id: string, answer: Answer) => (voteAnswer(id, answer, 1));
+export const downvoteAnswer = (id: string, answer: Answer) => (voteAnswer(id, answer, -1));
